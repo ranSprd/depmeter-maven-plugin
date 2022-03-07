535b0716347c4da32d7afebff0ea9fade6d2703c
Raik Nagel <raik.nagel@power.cloud>
2022-03-07 16:42:02 +0100

    collect upgrades

--- src/main/java/net/kiar/maven/dependency/freshness/UpgradableDependencies.java
+++ src/main/java/net/kiar/maven/dependency/freshness/UpgradableDependencies.java
@@ -1,13 +1,9 @@
 package net.kiar.maven.dependency.freshness;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import org.apache.maven.artifact.versioning.ArtifactVersion;
+import java.util.stream.Collectors;
 import org.apache.maven.model.Dependency;
 import org.codehaus.mojo.versions.api.ArtifactVersions;
-import org.codehaus.mojo.versions.api.UpdateScope;
 
 /**
  *
@@ -15,35 +11,17 @@ import org.codehaus.mojo.versions.api.UpdateScope;
  */
 public class UpgradableDependencies {
     
-    private static final boolean allowSnapshots = true;
-    
     
     public static UpgradableDependencies select(Map<Dependency, ArtifactVersions> dependencies) {
         
-        List<String> withUpdates = new ArrayList<>();
-        List<String> usingCurrent = new ArrayList<>();
-        Iterator i = dependencies.values().iterator();
-        while (i.hasNext()) {
-            ArtifactVersions versions = (ArtifactVersions) i.next();
-            ArtifactVersion latest;
-            if (versions.isCurrentVersionDefined()) {
-                latest = versions.getNewestUpdate(UpdateScope.ANY, allowSnapshots);
-            } else {
-                ArtifactVersion newestVersionInRange
-                        = versions.getNewestVersion(versions.getArtifact().getVersionRange(), allowSnapshots);
-                latest = newestVersionInRange == null ? null
-                        : versions.getNewestUpdate(newestVersionInRange, UpdateScope.ANY, allowSnapshots);
-                if (latest != null
-                        && ArtifactVersions.isVersionInRange(latest, versions.getArtifact().getVersionRange())) {
-                    latest = null;
-                }
-            }
-            
-            
-        }
+        dependencies.entrySet().stream()
+                .map(entry -> UpgradableDependency.create(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
+        
         
         
         return new UpgradableDependencies();
     }
+
     
 }
--- src/main/java/net/kiar/maven/dependency/freshness/UpgradableDependency.java
+++ src/main/java/net/kiar/maven/dependency/freshness/UpgradableDependency.java
@@ -1,7 +1,9 @@
 package net.kiar.maven.dependency.freshness;
 
 import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.apache.maven.model.Dependency;
 import org.codehaus.mojo.versions.api.ArtifactVersions;
+import org.codehaus.mojo.versions.api.UpdateScope;
 
 /**
  *
@@ -9,9 +11,74 @@ import org.codehaus.mojo.versions.api.ArtifactVersions;
  */
 public class UpgradableDependency {
     
-    private ArtifactVersions allVersions;
-    private ArtifactVersion latest;
+    private static final boolean allowSnapshots = true;
+    
+    
+    private final ArtifactVersions allVersions;
+    private final Dependency dependency;
+    
+    private ArtifactVersion latestVersion;
+    private ArtifactVersion usedVersion;
+    
+    
+    /**
+     * Create an instance and compute the current version and the latest available.
+     * @param dependency
+     * @param versions
+     * @return 
+     */
+    public static UpgradableDependency create(Dependency dependency, ArtifactVersions versions) {
+        UpgradableDependency result = new UpgradableDependency(dependency, versions);
+        if (versions.isCurrentVersionDefined()) {
+            result.setUsedVersion( versions.getCurrentVersion());
+            result.setLatestVersion( versions.getNewestUpdate(UpdateScope.ANY, allowSnapshots));
+        } else {
+            ArtifactVersion latest = null;
+            final ArtifactVersion newestVersionInRange
+                    = versions.getNewestVersion(versions.getArtifact().getVersionRange(), allowSnapshots);
+            if (newestVersionInRange != null) {
+                latest = versions.getNewestUpdate(newestVersionInRange, UpdateScope.ANY, allowSnapshots);
+                if (latest != null && ArtifactVersions.isVersionInRange(latest, versions.getArtifact().getVersionRange())) {
+                    latest = null;
+                }
+            }
+            result.setUsedVersion( newestVersionInRange);
+            result.setLatestVersion( latest);
+        }
+        
+        return result;
+    }
+    
 
+    public UpgradableDependency(Dependency dependency, ArtifactVersions allVersions) {
+        this.allVersions = allVersions;
+        this.dependency = dependency;
+    }
+
+    public ArtifactVersions getAllVersions() {
+        return allVersions;
+    }
+
+    public Dependency getDependency() {
+        return dependency;
+    }
+
+    public ArtifactVersion getLatestVersion() {
+        return latestVersion;
+    }
+
+    public void setLatestVersion(ArtifactVersion latestVersion) {
+        this.latestVersion = latestVersion;
+    }
+
+    public ArtifactVersion getUsedVersion() {
+        return usedVersion;
+    }
+    
+    public void setUsedVersion(ArtifactVersion usedVersion) {
+        this.usedVersion = usedVersion;
+    }
+    
     public boolean isUpgradable() {
         return true;
     }
